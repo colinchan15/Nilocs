5a0bfa0a11c2629a6453105e73ce41dc59d3e800
Colin MBP <cchan827@uwo.ca>
2018-10-22 11:30:11 -0400

    changed _forms.html.erb format to look nicer

--- app/models/cube.rb
+++ app/models/cube.rb
@@ -5,10 +5,10 @@ class Cube < ApplicationRecord
 	mount_uploader :image, AvatarUploader # for carrierwave
 	serialize :image, JSON # ONLY FOR SQLITE
 
-	validate :title, :brand, :model, :price, presence: true # validates that these line items are non-null
-	validate :description, length: {maximum: 1000, too_long: "%{count} characters is the maximum # of chars allowed... cmon man..."}
-	validate :title, length: {maximum: 100, too_long: "bro why is your title %{count} chars long... give me a new title"}
-	validate :price, length: {maximum: 7}
+	validates :title, :brand, :model, :price, presence: true # validates that these line items are non-null
+	validates :description, length: {maximum: 1000, too_long: "%{count} characters is the maximum # of chars allowed... cmon man..."}
+	validates :title, length: {maximum: 100, too_long: "bro why is your title %{count} chars long... give me a new title"}
+	validates :price, length: {maximum: 7}
 
 	BRAND = %w{Moyu Gans Valk Yuxin Dayan Cubicle FangShi}
 	FINISH = %w{Matte Glossy Solid Metallic Pearlescent Special}
