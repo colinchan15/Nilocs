ea35a6050f9eec2252145f7b46870d03cdd5e7db
Colin MBP <cchan827@uwo.ca>
2018-10-22 05:55:39 -0400

    still working on cube models

--- app/models/cube.rb
+++ app/models/cube.rb
@@ -1,2 +1,12 @@
 class Cube < ApplicationRecord
+	mount_uploader :image, AvatarUploader # for carrierwave
+	serialize :image, JSON # ONLY FOR SQLITE
+	belongs_to :user, optional: true
+
+	validate :title, :brand, :model, :price, presence: true # validates that these line items are non-null
+	validate :description, length: {maximum: 1000, too_long: "%{count} characters is the maximum # of chars allowed... cmon man..."}
+	validate :title, length: {maximum: 100, too_long: "bro why is your title %{count} chars long... give me a new title"}
+	validate :price, length: {maximum: 7}
+
+	
 end
