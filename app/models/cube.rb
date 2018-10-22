7c6a861de0e5d15614b4aa2cd6a1bbc5f9225fde
Colin MBP <cchan827@uwo.ca>
2018-10-22 11:17:44 -0400

    made changes to the models

--- app/models/cube.rb
+++ app/models/cube.rb
@@ -1,12 +1,16 @@
 class Cube < ApplicationRecord
+	
+	belongs_to :user, optional: true
+
 	mount_uploader :image, AvatarUploader # for carrierwave
 	serialize :image, JSON # ONLY FOR SQLITE
-	belongs_to :user, optional: true
 
 	validate :title, :brand, :model, :price, presence: true # validates that these line items are non-null
 	validate :description, length: {maximum: 1000, too_long: "%{count} characters is the maximum # of chars allowed... cmon man..."}
 	validate :title, length: {maximum: 100, too_long: "bro why is your title %{count} chars long... give me a new title"}
 	validate :price, length: {maximum: 7}
 
-	
+	BRAND = %w{Moyu Gans Valk Yuxin Dayan Cubicle FangShi}
+	FINISH = %w{Matte Glossy Solid Metallic Pearlescent Special}
+	CONDITION = %w{New Excellent Mint Used Fair Poor}
 end
